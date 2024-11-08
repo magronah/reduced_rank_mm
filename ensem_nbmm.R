3d9325a08f6426c3f09e7ff986b48711d493051a
magronah <lampofgod7@gmail.com>
2024-11-08 15:14:09 -0500

    added zinbmm code

--- ensem_nbmm.R
+++ ensem_nbmm.R
@@ -1,21 +1,25 @@
-setwd("/project/6006158/agronahm/Michael-n-Ben-Repo/")
 library(NBZIMM)
-path = paste0(getwd(),"/reproducible/new_sim/100_300/")
+library(DESeq2)
+path = paste0(getwd(),"/100_300/")
 ####################################################################
-data      =   readRDS(paste0(path,"otu_meta_list_nozi.rds"))
+data	  =   readRDS(paste0(path,"otu_meta_list_withzi_taxa.rds"))
 ################################################################
-cc      =   commandArgs(trailingOnly  = TRUE)
-i       =   as.integer(cc[1]) 
-dd      =   data[[i]]
+cc	=   commandArgs(trailingOnly  = TRUE)
+i	=   as.integer(cc[1])
+dd	=   data[[i]]
 ################################################################
 countdata  =   dd$countdata
 met_dd     =   dd$met_data
 met_dd$dummy = factor(1)
 
-mod        =   mms(y = countdata, fixed = ~group,
-                   random = ~ 1|dummy,
+otu_count    =   t(countdata)
+dds        =   DESeqDataSetFromMatrix(otu_count,met_dd, ~group)
+dds        =   DESeq(dds,sfType ="poscounts",minReplicatesForReplace=Inf)
+normalizer =   sizeFactors(dds)
+
+mod        =   mms(y = countdata, fixed = ~group + offset(normalizer),
+                   random =  ~ 1|dummy,
                    data = met_dd, method = "nb")
 
-saveRDS(mod, file=paste0("~/scratch/dataset/new_sim/100_300/nbmm_mod/mod",i, ".rds"))   
-
+saveRDS(mod, file=paste0("~/scratch/dataset/RR/100_300/nbmm/mod",i,".rds"))
 
