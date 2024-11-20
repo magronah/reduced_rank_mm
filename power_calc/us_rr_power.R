becbdbde75f1e439cf088434a995f15eb9cc0498
magronah <lampofgod7@gmail.com>
2024-11-20 12:19:09 -0500

    added gam fit for other models

--- power_calc/us_rr_power.R
+++ power_calc/us_rr_power.R
@@ -1,18 +1,20 @@
 library(tibble)
 library(DESeq2)
 library(Matrix)
+library(dplyr)
 library(huge)
 library(foreach)
 library(scam)
-
+library(tidyverse)
+#############################################
 source("/project/6006158/agronahm/reduced_rank_mm/func2.R")
 source("/project/6006158/agronahm/reduced_rank_mm/initial_param0.R")
 
 # Define sets of nsubj and ntaxa
 parameter_sets <- list(
-  list(nsubj = 50, ntaxa = 200),
-  list(nsubj = 100, ntaxa = 300),
-  list(nsubj = 150, ntaxa = 500)
+  list(nsubj = 50, ntaxa = 200)
+ #list(nsubj = 100, ntaxa = 300),
+ #list(nsubj = 150, ntaxa = 500)
 )
 
 # Loop through parameter sets
@@ -21,7 +23,7 @@ for (params in parameter_sets) {
   ntaxa <- params$ntaxa
   
   # Set file paths
-  path <- paste0("/project/6006158/agronahm/reduced_rank_mm/", nsubj, "_", ntaxa)
+  path <- paste0("/project/6006158/agronahm/reduced_rank_mm/", nsubj, "_", ntaxa, "/")
   
   # Load data and extract parameters
   dd <- load_data(path)
@@ -33,14 +35,14 @@ for (params in parameter_sets) {
   names(pvals) <- c("rr", "rrzi", "us", "uszi")
   
   # Read mean count data
-  mean_count <- readRDS(paste0(path, "/mean_count.rds"))
+  mean_count <- readRDS(paste0(path, "mean_count.rds"))
   
   # Fit GAM models for each p-value set
   models <- lapply(pvals, gam_fit, effect_size, mean_count, grid_len = 500, alpha_level = 0.05)
   names(models) <- c("rr", "rrzi", "us", "uszi")
   
   # Create output directory if it doesn't exist
-  file_path <- paste0(path, "/GAM/")
+  file_path <- paste0(path, "GAM/")
   if (!dir.exists(file_path)) {
     dir.create(file_path, recursive = TRUE)
     cat("Folder created at:", file_path, "\n")
@@ -54,7 +56,7 @@ for (params in parameter_sets) {
   })
   
   # Create output directory if it doesn't exist
-  file_path2 <- paste0(path, "/pvlaues/")
+  file_path2 <- paste0(path, "pvalues/")
   if (!dir.exists(file_path2)) {
     dir.create(file_path2, recursive = TRUE)
     cat("Folder created at:", file_path2, "\n")
@@ -64,6 +66,6 @@ for (params in parameter_sets) {
   
   # Save pvalues
   lapply(names(pvals), function(name) {
-    saveRDS(models[[name]], file = paste0(file_path, name, ".rds"))
+    saveRDS(pvals[[name]], file = paste0(file_path2, name, ".rds"))
   })
 }
