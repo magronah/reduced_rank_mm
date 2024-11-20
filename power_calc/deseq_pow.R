b220d9a2652434f4f1de7d138063a1ff0f72671b
magronah <lampofgod7@gmail.com>
2024-11-20 10:07:59 -0500

    added power calculations

--- power_calc/deseq_pow.R
+++ power_calc/deseq_pow.R
@@ -3,27 +3,16 @@ library(DESeq2)
 library(Matrix)
 library(huge)
 library(foreach)
-source("func2.R")
-source("initial_param0.R")
+source("/project/6006158/agronahm/reduced_rank_mm/func2.R")
+source("/project/6006158/agronahm/reduced_rank_mm/initial_param0.R")
 ############################################################
-path  =  "/project/6006158/agronahm/reduced_rank_mm/"
-pval  =  readRDS(dd, file = paste0(path,nsubj,"_",ntaxa,"/pvalues/deseq.rds"))
+path  =  paste0("/project/6006158/agronahm/reduced_rank_mm/",nsubj,"_",ntaxa)
+
+pval  =  readRDS(paste0(path,"/pvalues/deseq.rds"))
 ####################################################################
-dd        =   readRDS(paste0(path,nsubj,"_",ntaxa,"otu_meta_list_withzi_taxa.rds"))
-effect    =   readRDS(paste0(path,nsubj,"_",ntaxa,"true_param.rds"))
-mean_count  =  rowMeans(dd)
+data        =   readRDS(paste0(path,"/otu_meta_list_withzi_taxa.rds"))
+effect      =   readRDS(paste0(path,"/true_param.rds"))
+effect      =   effect$true_param
+mean_count  =  colMeans(do.call(rbind,lapply(data, function(x) (x$countdata))))
 ############################################################
 
-
-
-files   =   list.files(path, full.names = TRUE)
-res = foreach(i = files, .combine = "cbind") %do% {
-  res        =    readRDS(i)
-  res$padj
-}
-
-res            =    as.data.frame(res)
-colnames(res)  =    paste0("nsim", 1:ncol(res))
-rownames(res)  =    paste0("taxon",1:ntaxa)
-
-saveRDS(dd, file = paste0(getwd(),"/",nsubj,"_",ntaxa,"pvalues/deseq.rds"))
