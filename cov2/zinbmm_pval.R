3288338c0916b76152a49724eeaf28ec81ac2381
Michael Agronah <agronahm@cedar1.int.cedar.computecanada.ca>
2025-01-12 15:53:10 -0800

    added zinbmm_pval.rds

--- cov2/zinbmm_pval.R
+++ cov2/zinbmm_pval.R
@@ -7,18 +7,18 @@ library(Matrix)
 source("func2.R")
 source("initial_param0.R")
 ############################################################
-path = paste0("~/scratch/dataset/RR/coverage/",nsubj,"_",ntaxa,"/zinbmm2")
+path = paste0("~/scratch/dataset/RR/coverage/",200,"_",600,"/zinbmm2")
 path
 
 files <-   list.files(path, full.names = TRUE)
 
 res = foreach(i = files, .combine ="cbind") %do% {
   mod  =   readRDS(i)
-  lapply(mod,function(x) {summary(x)$coefficients$cond[, "Pr(>|z|)"][["grouptreat"]]})
+  unlist(lapply(mod,function(x) {summary(x)$coefficients$cond[, "Pr(>|z|)"][["grouptreat"]]}))
 }
 
 
 dd            =  as.data.frame(res)
 rownames(dd)  =  paste0("taxon",1:nrow(dd))
 colnames(dd)  =  paste0("sim",1:ncol(dd))
-saveRDS(dd, file = paste0("cov2/",nsubj,"_",ntaxa,"/pval_zinbmm.rds"))
+saveRDS(dd, file = paste0("cov2/",nsubj,"_",ntaxa,"/zinbmm_pval.rds"))
