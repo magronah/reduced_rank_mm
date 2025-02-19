5b00fee6c3faca82a37e3ac5431f277eeb08aa02
Michael Agronah <agronahm@cedar5.int.cedar.computecanada.ca>
2025-02-19 10:55:05 -0800

    added other model for confint calculation

--- deseq.R
+++ deseq.R
@@ -2,25 +2,28 @@ library(tibble)
 library(DESeq2)
 library(Matrix)
 library(huge)
+library(here)
 
 source("func2.R")
 source("initial_param0.R")
 
-path = paste0(getwd(),"/",nsubj,"_",ntaxa,"/")
+path = paste0(nsubj,"_",ntaxa,"/")
 path
 ####################################################################
 data      =   readRDS(paste0(path,"otu_meta_list_withzi_taxa.rds"))
 ################################################################
-cc      =   commandArgs(trailingOnly  = TRUE)
-i       =   as.integer(cc[1]) 
+#cc      =   commandArgs(trailingOnly  = TRUE)
+#i       =   as.integer(cc[1]) 
+
+for(i in 51:150){
 dd      =   data[[i]]
 ################################################################
 countdata  =   dd$countdata
 met_dd     =   dd$met_data
 
-res        =   deseqfun(countdata,met_dd,ref_name="control")
+res        =   deseqfun(countdata,met_dd,ref_name="control",ntaxa=ntaxa)
 
-file_path  =  paste0("~/scratch/dataset/RR","/",nsubj,"_",ntaxa,"/","deseq/")
+file_path  =  paste0("~/scratch/data/",nsubj,"_",ntaxa,"/","deseq/")
 
 if (!dir.exists(file_path)) {
   dir.create(file_path, recursive = TRUE)
@@ -32,4 +35,4 @@ if (!dir.exists(file_path)) {
 
 
 saveRDS(res, file=paste0(file_path,"mod",i,".rds"))
-
+}
